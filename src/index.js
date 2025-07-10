73f0ff21bb2d68ad97d342c89056aa8303fd6b05
EliasBach <el.bach1999@gmail.com>
2025-07-10 11:15:16 +0200

    search bar, button, display of data, some styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import "./styles.css";
-
-let search_term = "berlin"
-const API_key = "477T375DGMGEH24S6FDPG3JV3"
 // key is public and does not need to be hidden
+const API_key = "477T375DGMGEH24S6FDPG3JV3"
+const userinput = document.querySelector("#userinput")
+const search_button = document.querySelector(".searchbutton")
+search_button.addEventListener("click", fetchWeatherData)
+const description = document.querySelector("#description")
+const box_weather = document.querySelector("#current-conditions")
+const box_temp = document.querySelector("#temperature")
+const box_precip = document.querySelector("#expected-precipitation")
+const box_extra = document.querySelector("#extra-info")
 
-const request = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search_term}?key=${API_key}`
-
-function fetchWeatherData(request) {
-    fetch(request, {mode: 'cors'})
+function fetchWeatherData() {
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${userinput.value}?key=${API_key}`, {mode: 'cors'})
         .then(function(response) {
         return response.json()
         })
@@ -22,10 +26,16 @@ function fetchWeatherData(request) {
         });
 } 
 
-function processWeatherData(response) {
-    console.log(response)
-    console.log(response.days[0].tempmax)
+function processWeatherData(data) {
+    console.log(data)
+    description.textContent = `Current weather in ${data.resolvedAddress}`
+    box_weather.textContent = `${data.currentConditions.conditions}`
+    let tempC = tempFtoC(data.currentConditions.temp).toFixed(1)
+    box_temp.textContent = `${tempC} °C`
+    box_precip.textContent = `${data.currentConditions.precip}`
+    box_extra.textContent = `UV-Index: ${data.currentConditions.uvindex}`
 }
 
-// testing
-fetchWeatherData(request)
+function tempFtoC(tempF) {
+    return ((tempF-32) * (5/9))
+}
